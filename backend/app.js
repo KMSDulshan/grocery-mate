0fed7eea6d61b5cca2a8d7a16fc621389ac49539
K.M.S Dulshan <152279802+KMSDulshan@users.noreply.github.com>
2025-03-26 17:58:42 +0530

    app.js backend commit to dev

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const supplierRoutes = require("./Routes/SupplierRoute"); // Ensure correct path
 
 const app = express();
-
+const cors = require("cors");
 // Middleware
 app.use(express.json()); // ✅ Enables JSON parsing
-
+app.use(cors());
 // Use Supplier Routes
 app.use("/suppliers", supplierRoutes);
 
@@ -15,6 +15,6 @@ mongoose
   .connect("mongodb+srv://boot:boot@cluster0.kcv7y.mongodb.net/grocery-mate?retryWrites=true&w=majority")
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(5000, () => console.log("Server running on port 5000"));
+    app.listen(5001, () => console.log("Server running on port 5001"));
   })
   .catch((err) => console.error("MongoDB Connection Error:", err));
