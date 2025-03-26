bbdca2f4dd9bc079e10d2be3c15ba61d088c551f
K.M.S Dulshan <152279802+KMSDulshan@users.noreply.github.com>
2025-03-26 17:59:45 +0530

    suplier route commit to dev

--- backend/Routes/SupplierRoute.js
+++ backend/Routes/SupplierRoute.js
@@ -4,10 +4,15 @@ const  router = express.Router();
 //Insert Model
 const Supplier = require("../Model/SupplierModel");
 //Insert Controller
-const SupplierController = require("../Controlers/SupplierControle");
+const SupplierController = require("../Controllers/SupplierControllers");
 
 router.get("/",SupplierController.getAllSuppliers);
 router.post("/",SupplierController.addsupplier );
 router.get("/:id",SupplierController.getById );
+router.put("/:id",SupplierController.updatesupplier);
+router.delete("/:id",SupplierController.deletesupplier);
+
+
+
 //export
 module.exports = router;
