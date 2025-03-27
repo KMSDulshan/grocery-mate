17505f1a5fd6e3d4e6694ed824334a510d078eb6
K.M.S Dulshan <152279802+KMSDulshan@users.noreply.github.com>
2025-03-27 16:21:53 +0530

    feet update

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,6 @@ mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(5001, () => console.log("Server running on port 5001"));
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
   })
   .catch((err) => console.error("MongoDB Connection Error:", err));
--- backend/src/testConnection.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const connectDB = require('./config/db');
-
-connectDB().then(() => {
-  console.log('Connection test successful');
-  process.exit(0);
-});
