a1da8b3aa743863604b1b8dbc7593d681db3fe59
Amaa0915 <153179645+Amaa0915@users.noreply.github.com>
2025-05-04 16:30:04 +0530

    Add route to the file

---
+++ backend/src/routes/inventory.route.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router();
+const InventoryController = require("../controllers/inventory.controller");
+
+router.get("/", InventoryController.getAllInventory);
+router.post("/", InventoryController.addInventory);
+router.get("/:id", InventoryController.getById);
+router.put("/:id", InventoryController.updateInventory);
+router.delete("/:id", InventoryController.deleteInventory);
+
+module.exports = router;
+
+//get - display the details
+//post - insert
+//put - Upfate
